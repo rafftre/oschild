96325741d8611cfda8fcc1449e0742599ca98830
Raffaele Tretola <rafftre@hey.com>
2024-04-02 13:09:53 +0200

    Managed state transitions

--- lib/child.ex
+++ lib/child.ex
@@ -1,18 +1,36 @@
 defmodule Child do
   @moduledoc """
-  Documentation for `Child`.
+  A 4-year-old child.
   """
+
+  @type action :: :play | :eat | :kindergarten
+
+  @type state :: :playing | :eating | :hiding
+
+  @type t :: %__MODULE__{
+          state: state
+        }
+  defstruct(state: :playing)
+
+  @doc "Wake up a new child ready to play."
+  @spec wake_up() :: t
+  def wake_up, do: %__MODULE__{}
 
   @doc """
-  Hello world.
+  Call a child to perform an action.
 
-  ## Examples
-
-      iex> Child.hello()
-      :world
-
+  Raise an error if the action causes an invalid transition.
   """
-  def hello do
-    :world
+  @spec call_to!(t, action) :: t
+  def call_to!(child = %{state: state}, action) do
+    case state_transition(state, action) do
+      {:ok, new_state} -> %__MODULE__{child | state: new_state}
+      {:error} -> raise("Impossible transition")
+    end
   end
+
+  defp state_transition(_state, :kindergarten), do: {:ok, :hiding}
+  defp state_transition(_state = :playing, :eat), do: {:ok, :eating}
+  defp state_transition(state, :play) when state in [:eating, :hiding], do: {:ok, :playing}
+  defp state_transition(_, _), do: {:error}
 end
--- test/child_test.exs
+++ test/child_test.exs
@@ -2,7 +2,34 @@ defmodule ChildTest do
   use ExUnit.Case
   doctest Child
 
-  test "greets the world" do
-    assert Child.hello() == :world
+  test "just woken up" do
+    assert Child.wake_up() == %Child{state: :playing}
+  end
+
+  test "playing transitions" do
+    playing = Child.wake_up()
+    assert playing == %Child{state: :playing}
+
+    assert Child.call_to!(playing, :eat) == %Child{state: :eating}
+    assert Child.call_to!(playing, :kindergarten) == %Child{state: :hiding}
+    assert_raise RuntimeError, fn -> Child.call_to!(playing, :play) end
+  end
+
+  test "eating transitions" do
+    eating = Child.wake_up() |> Child.call_to!(:eat)
+    assert eating == %Child{state: :eating}
+
+    assert Child.call_to!(eating, :play) == %Child{state: :playing}
+    assert Child.call_to!(eating, :kindergarten) == %Child{state: :hiding}
+    assert_raise RuntimeError, fn -> Child.call_to!(eating, :eat) end
+  end
+
+  test "hiding transitions" do
+    hiding = Child.wake_up() |> Child.call_to!(:kindergarten)
+    assert hiding == %Child{state: :hiding}
+
+    assert Child.call_to!(hiding, :play) == %Child{state: :playing}
+    assert Child.call_to!(hiding, :kindergarten) == %Child{state: :hiding}
+    assert_raise RuntimeError, fn -> Child.call_to!(hiding, :eat) end
   end
 end
