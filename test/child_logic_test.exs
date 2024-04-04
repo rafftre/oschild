a5d970677dec57a4138a61221f83a45cf44bf4c4
Raffaele Tretola <rafftre@hey.com>
2024-04-04 15:50:38 +0200

    Extended tests

--- test/child_logic_test.exs
+++ test/child_logic_test.exs
@@ -33,4 +33,19 @@ defmodule ChildLogicTest do
     assert Child.call_to!(hiding, :kindergarten) == %Child{state: :hiding}
     assert_raise RuntimeError, fn -> Child.call_to!(hiding, :eat) end
   end
+
+  test "invalid calls" do
+    playing = Child.wake_up()
+    assert playing == %Child{state: :playing}
+
+    assert_raise FunctionClauseError, fn -> Child.call_to!("aaa", :eat) end
+    assert_raise FunctionClauseError, fn -> Child.call_to!(1, :eat) end
+    assert_raise FunctionClauseError, fn -> Child.call_to!(false, :eat) end
+    assert_raise FunctionClauseError, fn -> Child.call_to!(nil, :eat) end
+
+    assert_raise RuntimeError, fn -> Child.call_to!(playing, "aaa") end
+    assert_raise RuntimeError, fn -> Child.call_to!(playing, 1) end
+    assert_raise RuntimeError, fn -> Child.call_to!(playing, false) end
+    assert_raise RuntimeError, fn -> Child.call_to!(playing, nil) end
+  end
 end
